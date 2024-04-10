842c6f90700d361532db05d148511a01bd52157b
kuzhalikumaraguru <kuzhali.kumaraguru@gmail.com>
2024-04-10 16:32:25 +0530

    backend deployed url added

--- src/Routes/AxiosService.jsx
+++ src/Routes/AxiosService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const AxiosService = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://capstonebackend-1ast.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
